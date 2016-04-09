04bfeb9b3dfdc3515fed68f5495767743a45d675
Roman Lorent <romanlorent@gmail.com>
2016-04-09 13:11:08 +0100

    1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("test");
+var express = require('express');
+var app = express();
+var path = require("path");
+
+
+app.use(function (req, res, next) {
+    var err = "";
+    if (err) {
+        console.log(err);
+    } else {
+        res.sendFile(path.join(__dirname + '/index.html'));
+        next();
+    }
+});
+
+
+app.use('/d', function (req, res) {
+
+
+    console.log("done");
+
+
+});
+
+
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
